1fbfef8c5c565449009b9f4c7c2185abaa9fd0ae
nguyentrungkien2180606198 <kienadu123@gmail.com>
2024-04-03 20:13:36 +0700

    sua

--- BaiThucHanh/Controllers/HomeController.cs
+++ BaiThucHanh/Controllers/HomeController.cs
@@ -28,7 +28,6 @@ namespace BaiThucHanh.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        /////////////////////dadaaaaaaaaaaaaaaassakijadaaaaaaaaaaaa
-        ///
+        //sua1
         }
 }
